068d49477980a44e91953d33dbbcb6528ce3f9a2
Brian Seo <98533321+BrianYSeo@users.noreply.github.com>
2022-02-14 17:11:39 +0000

    Fixed Errors

--- main.py
+++ main.py
@@ -5,12 +5,14 @@ classes = 5
 tuition = 6000
 cost_per_week = ((tuition / classes) / weeks)
 print("Cost per week:", cost_per_week)
+print(cost_per_week, type(cost_per_week))
 classes_per_week = 3
+print(classes_per_week, type(classes_per_week))
 cost_per_class = (cost_per_week / classes_per_week)
 print("Cost per class:", cost_per_class)
-
+print(cost_per_class, type(cost_per_class))
 
 #Part B
-List = [1, 2, 3, 4, 5]
-result = random.choice(List)
+list1 = [1, 2, 3, 4, 5]
+result = random.choice(list1)
 print("result:", result)
